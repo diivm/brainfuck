12d0f207614d83c92fa20e15ef7678cfacbafc0c
Divyanshu Madan <iamdm99@gmail.com>
2020-10-06 10:38:29 +0530

    capture stdout and test

--- test_interpret_0.py
+++ test_interpret_0.py
@@ -1,10 +1,19 @@
-import pytest
 import interpret_0
+
+import pytest
+import io
+from contextlib import redirect_stdout
 
 
 def test_hello_world():
-    f = open("input/hello_world.bf")
-    input_buffer = f.read()
+    input_buffer = open("input/hello_world.bf").read()
 
     python_code = interpret_0.interpret_0(input_buffer)
-    exec(python_code)
+
+    # Capture stdout from exec
+    f = io.StringIO()
+    with redirect_stdout(f):
+        exec(python_code)
+    out = f.getvalue()[:-1]  # out has '\n' at the end, remove that
+
+    assert out == "Hello World!"
